06335a39aca5781b5cc129fbd9762434364bfdf8
Wojciech Kubaty <wkubaty@gmail.com>
2018-05-06 21:29:21 +0200

    Some refactor

--- src/main/java/pl/edu/agh/sr/ExchangeOffice/ExchangeOfficeImpl.java
+++ src/main/java/pl/edu/agh/sr/ExchangeOffice/ExchangeOfficeImpl.java
@@ -1,6 +1,5 @@
 package pl.edu.agh.sr.ExchangeOffice;
 
-
 import io.grpc.stub.StreamObserver;
 import sr.grpc.gen.Currency;
 import sr.grpc.gen.CurrencyCode;
@@ -9,33 +8,18 @@ import sr.grpc.gen.ExchangeRateRequest;
 import sr.grpc.gen.ExchangeRateResponse;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
 
 public class ExchangeOfficeImpl extends ExchangeOfficeImplBase {
-    private int counter = 0;
     @Override
     public void subscribeToExchangeRatesChanges(ExchangeRateRequest request, StreamObserver<ExchangeRateResponse> responseObserver) {
         System.out.println("New bank subscribed...");
         List<CurrencyCode> supportedCurrencies = request.getSupportedCurrenciesList();
+        CurrencyCode defaultCurrency = request.getDefaultCurrency();
         while (true){
-            Map<CurrencyCode, CurrencyValue> updatedCurrencies = ExchangeRatesMonitor.getExchangeRates();
-            updatedCurrencies.forEach((k, v) -> System.out.println(v.getValue()));
-            List<Currency> currenciesToSend = new LinkedList<>();
-            supportedCurrencies.forEach(currencyCode -> {
-                if(updatedCurrencies.get(currencyCode).isUpdated()){
-                    currenciesToSend.add(
-                            Currency.newBuilder()
-                                    .setCurrencyCode(currencyCode)
-                                    .setValue(ExchangeRatesMonitor.getExchangeRates().get(currencyCode).getValue().toString())
-                                    .build());
-                }
-            });
-            System.out.println("size: " + currenciesToSend.size());
-            ExchangeRateResponse response = ExchangeRateResponse.newBuilder()
-                    .addAllCurrencies(currenciesToSend)
-                    .build();
+            ExchangeRateResponse response = createResponseWithUpdatedCurrencies(supportedCurrencies, defaultCurrency);
             responseObserver.onNext(response);
-
             try{
                 Thread.sleep(1000);
             }
@@ -45,8 +29,27 @@ public class ExchangeOfficeImpl extends ExchangeOfficeImplBase {
         }
         //responseObserver.onCompleted();
     }
-    private int getNextValue(){
-        counter++;
-        return counter;
+    private ExchangeRateResponse createResponseWithUpdatedCurrencies(List<CurrencyCode> supportedCurrencies, CurrencyCode defaultCurrency){
+        Map<CurrencyCode, CurrencyValue> updatedExchangeRates =
+                ExchangeRatesMonitor.getExchangeRates()
+                        .entrySet()
+                        .stream()
+                        .filter(e -> e.getValue().isUpdated())
+                        .collect(Collectors.toConcurrentMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        List<Currency> currenciesToSend = new LinkedList<>();
+        supportedCurrencies.forEach(currencyCode -> {
+            if(updatedExchangeRates.containsKey(currencyCode)){
+                currenciesToSend.add(
+                        Currency.newBuilder()
+                                .setCurrencyCode(currencyCode)
+                                .setValue(ExchangeRatesMonitor.convertExchangeRatesToRequestedCurrency(currencyCode, defaultCurrency).toString())
+                                .build());
+            }
+        });
+        return ExchangeRateResponse.newBuilder()
+                .addAllCurrencies(currenciesToSend)
+                .build();
+
     }
 }
--- src/main/java/pl/edu/agh/sr/ExchangeOffice/ExchangeRatesMonitor.java
+++ src/main/java/pl/edu/agh/sr/ExchangeOffice/ExchangeRatesMonitor.java
@@ -1,13 +1,9 @@
 package pl.edu.agh.sr.ExchangeOffice;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import sr.grpc.gen.CurrencyCode;
-
 import java.math.BigDecimal;
 import java.math.MathContext;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class ExchangeRatesMonitor implements Runnable{
     private static final double PROBABILITY_OF_CURRENCY_EXCHANGE_RATE_CHANGE = 0.5;
@@ -20,16 +16,9 @@ public class ExchangeRatesMonitor implements Runnable{
     }
 
     private static void initializeExchangeRates() {
-        exchangeRates.put(CurrencyCode.PLN, new CurrencyValue(new BigDecimal("0.2811"), true));
-        exchangeRates.put(CurrencyCode.USD, new CurrencyValue(new BigDecimal("0.3811"), true));
-        exchangeRates.put(CurrencyCode.EUR, new CurrencyValue(new BigDecimal("0.4811"), true));
-//        exchangeRates.put(new CurrencyValue(CurrencyCode.USD, new BigDecimal("1.0000")), true);
-//        exchangeRates.put(new CurrencyValue(CurrencyCode.EUR, new BigDecimal("1.1977")), true);
-//        exchangeRates.put(new CurrencyValue(CurrencyCode.GBP, new BigDecimal("1.3535")), true);
-//        exchangeRates.put(new CurrencyValue(CurrencyCode.CHF, new BigDecimal("1.0001")), true);
-//        exchangeRates.put(new CurrencyValue(CurrencyCode.JPY, new BigDecimal("0.0091")), true);
-//        exchangeRates.put(new CurrencyValue(CurrencyCode.RUB, new BigDecimal("0.0160")), true);
-//        exchangeRates.put(new CurrencyValue(CurrencyCode.CNY, new BigDecimal("0.1571")), true);
+        exchangeRates.put(DEFAULT_CURRENCY, new CurrencyValue(new BigDecimal("1.0000"), true));
+        exchangeRates.put(CurrencyCode.PLN, new CurrencyValue(new BigDecimal("0.28"), true));
+        exchangeRates.put(CurrencyCode.EUR, new CurrencyValue(new BigDecimal("1.1977"), true));
     }
 
     @Override
@@ -46,12 +35,15 @@ public class ExchangeRatesMonitor implements Runnable{
     }
 
     public static void updateExchangeRates() {
-        exchangeRates.forEach((k, v) -> exchangeRates.replace(k, simulateExchangeRateChanges(v)));
+        exchangeRates.forEach((k, v) -> exchangeRates.replace(k, simulateExchangeRateChanges(k, v)));
     }
 
-    private static CurrencyValue simulateExchangeRateChanges(CurrencyValue presentCurrencyValue){
+    private static CurrencyValue simulateExchangeRateChanges(CurrencyCode currencyCode, CurrencyValue presentCurrencyValue){
         if(shouldCurrencyExchangeRateChange()){
-            double multiplicand = 0.9 + Math.random()/5.0; // between 0.9 and 1.1
+            if(currencyCode.equals(DEFAULT_CURRENCY)){
+                return new CurrencyValue(presentCurrencyValue.getValue(), true);
+            }
+            double multiplicand = 0.99 + Math.random() / 50.0; // between 0.99 and 1.01
             BigDecimal newValue = presentCurrencyValue.getValue().multiply(new BigDecimal(multiplicand)).round(new MathContext(5));
             return new CurrencyValue(newValue, true);
         }
@@ -65,6 +57,12 @@ public class ExchangeRatesMonitor implements Runnable{
 
     public static ConcurrentHashMap<CurrencyCode, CurrencyValue> getExchangeRates() {
         return exchangeRates;
+    }
+
+    public static BigDecimal convertExchangeRatesToRequestedCurrency(CurrencyCode requestedCurrencyCode, CurrencyCode clientDefaultCurrencyCode) {
+        BigDecimal clientCurrencyValue = exchangeRates.get(clientDefaultCurrencyCode).getValue();
+        return exchangeRates.get(requestedCurrencyCode).getValue().divide(clientCurrencyValue, new MathContext(5));
+
     }
 
 }
